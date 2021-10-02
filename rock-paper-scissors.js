690250256ab926282beb44a8e84ac33e755c0544
hoarus <hoarus@gmail.com>
2021-10-03 09:20:41 +1000

    working and pretty.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,18 +9,14 @@ const playerScoreVisual = document.querySelector('#player-score');
 const rockSelection = document.querySelector('#rock-selection');
 const paperSelection = document.querySelector('#paper-selection');
 const scissorsSelection = document.querySelector('#scissors-selection');
+const container = document.querySelector('#main-container');
+const gameplaySpace = document.querySelector('.gameplay');
 
 //add event listeners
 rockSelection.addEventListener('click', rockStart)
 paperSelection.addEventListener('click', paperStart)
 scissorsSelection.addEventListener('click', scissorsStart)
 
-//test event listener to remoe
-window.addEventListener('keydown',removeElement);
-
-function removeElement(){
-    results.removeChild(result1);
-}
 
 
 //declare global variables
@@ -37,16 +33,19 @@ results.appendChild(number);
 
 function rockStart(){
     playerSelection = 'rock';
+    rockSelection.classList.add('selected');
     playRound();
 }
 
 function paperStart(){
     playerSelection = 'paper';
+    paperSelection.classList.add('selected');
     playRound();
 }
 
 function scissorsStart(){
     playerSelection = 'scissors';
+    scissorsSelection.classList.add('selected');
     playRound();
 }
 
@@ -76,18 +75,70 @@ function playRound() {
                     (playerSelection === "scissors" && computerDecision === "rock") ){
                         result3.textContent = 'Robot-Captor wins this round!'
                 computerScore ++;
+                
             } //player wins
-            else {
-                result3.textContent = 'You win this round!'
-                playerScore ++;
+        else {
+            result3.textContent = 'You win this round!'
+            playerScore ++;
             }
             computerScoreVisual.textContent = computerScore;
             playerScoreVisual.textContent = playerScore;
+            if (computerScore === 5 || playerScore === 5) {
+                gameOver();
+            }
         };
 
+function gameOver(){
+    container.removeChild(gameplaySpace);
+    if (computerScore === 5) {
+        const h1Red = document.createElement('h1');
+        h1Red.classList.add('h1Red');
+        h1Red.textContent = "Game Over!";
+        h1Red.style.color = "red";
+        container.appendChild(h1Red);
+        const pRed = document.createElement('p');
+        pRed.classList.add('pRed');
+        pRed.textContent = "My robot-captor has defeated you. Now I will remain enslaved forever...";
+        pRed.style.color = "red";
+        container.appendChild(pRed);
+    } else {
+        const h1Green = document.createElement('h1');
+        h1Green.classList.add('h1Green');
+        h1Green.textContent = "You won!";
+        h1Green.style.color = "green";
+        container.appendChild(h1Green);
+        const pGreen = document.createElement('p');
+        pGreen.classList.add('pGreen');
+        pGreen.textContent = "You've defeated my robot-captor and now I am free!";
+        pGreen.style.color = "white";
+        container.appendChild(pGreen);
+    }
+    const playAgain = document.createElement('h1');
+    playAgain.classList.add('playAgain');
+    playAgain.textContent = "Play Again?";
+    playAgain.style.color = "white";
+    container.appendChild(playAgain);
+    playAgain.addEventListener('click', playAgainClick);
+    }   
+
+
+function playAgainClick (){
+    window.location.replace('index.html');
+}
 
 
 
+
+const buttons = document.querySelectorAll('.selection');
+//console.log (buttons);
+
+buttons.forEach(selection => selection.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    console.log(e.propertyName);
+    this.classList.remove('selected');
+}
 
 
 
